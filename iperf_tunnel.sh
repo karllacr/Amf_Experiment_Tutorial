187c1c189b931343c6cb7a4041e439ffd8f2a178
Karlla Chaves <karllabiancachaves@gmail.com>
2022-08-01 16:35:46 -0300

    Update iperf_tunnel.sh

--- iperf_tunnel.sh
+++ iperf_tunnel.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+IP=$2
 QUANTIDADE=$1
 
 #executar o lado servidor do iperf em background
@@ -9,7 +10,7 @@ sudo docker exec -it upf iperf3 -s -D
 for i in $(seq $QUANTIDADE)
 do
 #irá executar o iperf através de cada um dos túneis criados
-   docker exec -d my5grantester iperf3 -c <ip_upf> -t 60 -B 10.60.0.$i 
+   docker exec -d my5grantester iperf3 -c $2 -t 60 -B 10.60.0.$i 
    echo 'Iperf iniciado no túnel uetun'$i' ...'
 done
 
