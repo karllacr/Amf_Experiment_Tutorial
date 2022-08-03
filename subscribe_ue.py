a767a53592cda42edad9c2c50b36d226c438a072
Karlla Chaves <karllabiancachaves@gmail.com>
2022-08-03 16:54:48 -0300

    Update subscribe_ue.py

--- subscribe_ue.py
+++ subscribe_ue.py
@@ -9,7 +9,6 @@ qtd_ue = int(sys.argv[1])
 with open('Amf_Experiment_Tutorial/base.json') as f:
   data_ue = json.load(f)
  
-print("AGUARDE POR ALGUNS SEGUNDOS...")
 
 for i in range(1, qtd_ue+1):
     formatted_number = str(i).zfill(10)
