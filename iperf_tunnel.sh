0e783d3239a43a1e269c694ac3f32b997a515d9c
Karlla Chaves <karllabiancachaves@gmail.com>
2022-08-07 11:03:15 -0300

    Update iperf_tunnel.sh

--- iperf_tunnel.sh
+++ iperf_tunnel.sh
@@ -2,23 +2,24 @@
 
 IP=$1
 QUANTIDADE=$2
-OUTPUT="$(docker exec my5grantester pgrep iperf)"
+OUTPUT="$(sudo docker exec my5grantester pgrep iperf)"
 
 for i in $(seq $QUANTIDADE)
 do
-
-   sudo docker exec upf iperf3 -s -p 52$QUANTIDADE
-   sudo docker exec -d my5grantester iperf3  --no-delay --client $IP --port 52$2 -t 60 --bind 10.60.0.$2 --interval 0 --parallel 5
-   
-   echo 'IPERF INICIADO NO TÚNEL UETUN'$i' ...'
+   sudo docker exec upf iperf3 -s -p 52$QUANTIDADE -D
+   echo 'SERVIDOR IPERF INICIADO NA PORTA 52'$i' DA UPF ...'
 done
 
-while : ; do
-      if  [[ -n $output ]]
-      then
-          printf "IPERF FINALIZADO!"
-      else 
-          printf "EXECUTANDO..."
-      fi
-    sleep 1
-done
+
+#sudo docker exec -d my5grantester iperf3  --no-delay --client $IP --port 52$2 -t 60 --bind 10.60.0.$2 --interval 0 --parallel 5
+#echo 'SERIVOR IPERF INICIADO NO TÚNEL UETUN'$i' ...'
+
+#while : ; do
+#      if  [[ -n $output ]]
+#      then
+#          printf "IPERF FINALIZADO!"
+#      else 
+#          printf "EXECUTANDO..."
+#      fi
+#    sleep 1
+#done
