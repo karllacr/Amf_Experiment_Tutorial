73f9d52a70285c956d615211f7815cbbffd55111
Karlla Chaves <karllabiancachaves@gmail.com>
2022-10-12 16:19:08 -0300

    Update subscribe_ue_assinc.py

--- subscribe_ue_assinc.py
+++ subscribe_ue_assinc.py
@@ -20,7 +20,7 @@ def request(i):
 
     header = {"Token" : "admin", "Content-Type" : "application/json"}
     r = requests.post('http://localhost:5000/api/subscriber/'+plmn_e_ue+'/20893',  data = json.dumps(data_ue), headers=header )
-    time.sleep(30)
+    time.sleep(5)
     print('UE ' + plmn_e_ue + ' SUBSCRIBED!' )
 
 
